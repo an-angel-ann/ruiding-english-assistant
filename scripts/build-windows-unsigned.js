8cce4173ff63db386f5797e7b21a09735882a23f
an-angel-ann <angela.wsr@gmail.com>
2025-11-05 06:22:24 +0800

    Fix: delete env vars instead of setting to undefined

--- scripts/build-windows-unsigned.js
+++ scripts/build-windows-unsigned.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 // Windows构建脚本 - 禁用代码签名
+delete process.env.WIN_CSC_LINK;
+delete process.env.WIN_CSC_KEY_PASSWORD;
+delete process.env.CSC_LINK;
+delete process.env.CSC_KEY_PASSWORD;
 process.env.CSC_IDENTITY_AUTO_DISCOVERY = 'false';
-process.env.WIN_CSC_LINK = undefined;
-process.env.WIN_CSC_KEY_PASSWORD = undefined;
 
 const { build } = require('electron-builder');
 
